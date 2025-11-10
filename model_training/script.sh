1111545123d31531952794bc6c30191f9c37d54c
Alexandros Angeli <alexandrosangeli@gmail.com>
2025-11-10 16:01:46 +0000

    Fix gpu argument not passed correctly

--- model_training/script.sh
+++ model_training/script.sh
@@ -2,5 +2,5 @@
 
 bash ../slurm.sh "python3 train.py \
     --output_dir=out \
-    --data_path="../drift/data/proteins/" \
-    --pdbs MurD_closed.pdb MurD_open.pdb"
+    --data_path=../drift/data/proteins/ \
+    --pdbs MurD_closed.pdb MurD_open.pdb" "$@" 
